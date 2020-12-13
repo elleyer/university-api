8eef9df3f02b36270c67064d84bc19166880d7e1
Elleyer <shudenkoslavik@gmail.com>
2020-12-13 19:37:06 +0200

    Context updated

--- API/Database/ApplicationContext.cs
+++ API/Database/ApplicationContext.cs
@@ -6,7 +6,7 @@ namespace Admin.Database
     public sealed class ApplicationContext : DbContext
     {
         public DbSet<Faculty> Faculties { get; set; }
-        public DbSet<Models.User.User> Users { get; set; }
+        public DbSet<Models.Mod.ModEntity> Moderators { get; set; }
         
         public DbSet<News> News { get; set; }
         
